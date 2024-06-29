3f3448922ee187ed38da2f4ec5be49759495c799
Suriyapraba1 <167987270+Suriyapraba1@users.noreply.github.com>
2024-06-29 15:40:07 +0530

    Add files via upload

---
+++ MainChrome.java
@@ -0,0 +1,24 @@
+package Task21;
+
+import org.junit.After;
+import org.junit.Before;
+import org.openqa.selenium.chrome.ChromeDriver;
+import java.time.Duration;
+
+public class MainChrome {
+
+    ChromeDriver chromeDriver;
+
+    @Before
+    public  void setup()
+    {
+        chromeDriver = new ChromeDriver();
+        chromeDriver.manage().window().maximize();
+        chromeDriver.manage().timeouts().implicitlyWait(Duration.ofSeconds(5));
+    }
+    @After
+    public  void down()
+    {
+        chromeDriver.quit();
+    }
+}
